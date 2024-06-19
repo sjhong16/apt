94662f65abd4b3c4e9fe53e3c56bb0fad3593924
mj.shin <killrain@gmail.com>
2024-06-19 22:26:42 +0900

    Update apt.js

--- apt.js
+++ apt.js
@@ -2,7 +2,7 @@
 // @name        네이버 부동산 테이블 뷰
 // @namespace   Violentmonkey Scripts
 // @match       https://new.land.naver.com/complexes*
-// @version     0.1
+// @version     0.1.1
 // @author      Maru
 // @description Please use with violentmonkey
 // @require     https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
