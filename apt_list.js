852df6e3fa125dc039e4b5d5744b48560ed3f014
sjhong16 <withsj@gmail.com>
2024-06-20 18:35:53 +0900

    Update and rename apt_v2.js to apt_list.js

--- apt_list.js
+++ apt_list.js
@@ -2,7 +2,7 @@
 // @name        네이버 부동산 매물 리스트
 // @namespace   Violentmonkey Scripts
 // @match       https://new.land.naver.com/complexes*
-// @version     0.1.9
+// @version     0.1.1
 // @author      Buhong
 // @description Please use with violentmonkey
 // @require     https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
@@ -32,8 +32,6 @@ class Item {
 let curentApt = "";
 let items = new Map();
 let dirty = false;
-
-// abcde
 
 // 아파트 이름 감시
 function observeMainTitle() {
