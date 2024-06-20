c0ee12c254739c624a8375648b6ee8194470b4d1
sjhong16 <withsj@gmail.com>
2024-06-20 16:40:26 +0900

    Update apt.js

--- apt.js
+++ apt.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        네이버 부동산 매물 리스트 v0.1
+// @name        네이버 부동산 매물 리스트 v0.1.5
 // @namespace   Violentmonkey Scripts
 // @match       https://new.land.naver.com/complexes*
-// @version     0.1.1.1
+// @version     0.1.5
 // @author      buhong
 // @description Please use with violentmonkey
 // @require     https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
@@ -12,11 +12,10 @@
  
 class Item {
     constructor() {
-        //this.aptname = "";
-        this.dong = "";
         this.type = "";
         this.price = "";
         this.monthly = "";
+        this.dong = "";
         this.floor = "";
         this.direction = "";
         this.area = "";
@@ -36,9 +35,7 @@ let dirty = false;
 
 // 아파트 이름 감시
 function observeMainTitle() {
-    //let title = $('#complexTitle')[0].innerText;
-    let title = $('#complexTitle').innerText;
- console.log(title); 
+    let title = $('#complexTitle')[0].innerText;
     if (title) {
         if (curentApt !== title) {
             curentApt = title;
@@ -47,63 +44,61 @@ function observeMainTitle() {
     }
 }
 
-// 
-
 // 각각의 매물을 감시한다.
 function observeItems() {
     let itemNodes = document.querySelectorAll('.item_list .item');
-        console.log(itemNodes.length); 
     if (!itemNodes) {
+        console.log(`no item node`);
         return;
     }
+    console.log(`item count: ${itemNodes.length}`);
 
     itemNodes.forEach(itemNode => {
-        let item = new Item();
-
-        // item_title (아파트명, 동)
-        //item.aptname = itemNode.querySelector('.item_title .text').innerText.split(' ')[0];
-        item.dong = itemNode.querySelector('.item_title .text').innerText.split(' ')[1];
-     
-        item.type = itemNode.querySelector('.price_line .type').innerText;
-    
-        let price = itemNode.querySelector('.price_line .price').innerText;
-        let prices = price.split("/");
-        item.price = prices[0];
-        if (prices.length > 1) {
-            item.monthly = prices[1];
-        } else {
-            item.monthly = "";
-        }
-    
-        let specs = itemNode.querySelector('.info_area .line:nth-child(1) .spec').innerText.split(', ');
-        item.area = specs[0];
-        item.floor = specs[1];
-        item.direction = specs[2];
-    
-        //item.desc = itemNode.querySelector('.info_area .line:nth-child(2) .spec').innerText;
-        item.realEstate = itemNode.querySelector('.agent_info:nth-child(2) .agent_name').innerText;
-        item.date = itemNode.querySelector('.label_area .label .data').innerText;
-    
-        let key = item.key();
-        if (!items.has(key)) {
-            items.set(key, item);
-            dirty = true;
+        try {
+            let item = new Item();
+            item.type = itemNode.querySelector('.price_line .type').innerText;
+            item.dong = itemNode.querySelector('.item_title .text').innerText.split(' ')[1];
+        
+            let price = itemNode.querySelector('.price_line .price').innerText;
+            let prices = price.split("/");
+            item.price = prices[0];
+            if (prices.length > 1) {
+                item.monthly = prices[1];
+            } else {
+                item.monthly = "";
+            }
+        
+            let specs = itemNode.querySelector('.info_area .line:nth-child(1) .spec').innerText.split(', ');
+            item.area = specs[0];
+            item.floor = specs[1];
+            item.direction = specs[2];
+        
+            item.desc = itemNode.querySelector('.info_area .line:nth-child(2) .spec').innerText;
+            item.realEstate = itemNode.querySelector('.agent_info:nth-child(2) .agent_name').innerText;
+            item.date = itemNode.querySelector('.label_area .label .data').innerText;
+        
+            let key = item.key();
+            if (!items.has(key)) {
+                items.set(key, item);
+                dirty = true;
+            }
+        } catch(e) {
+           console.error(e);
         }
     });
 }
 
 // tsv 파일 다운로드 만들기
 function tsvDownload() {
-    console.log(123); 
     const $node = $('.list_complex_info .complex_price_wrap');
     if (!$node) {
+        console.log(`no node`);
         return;
     }
 
-    let tsv = "날짜\t아파트\t타입\t가격\t월세\t동\t층\t면적\t방향\t설명\t부동산\n";
+    let tsv = "날자\t타입\t가격\t월세\t동\t층\t면적\t방향\t설명\t부동산\n";
     items.forEach((item) => {
         tsv += `${item.date}\t`
-            //+ `${item.aptname}\t`
             + `${item.type}\t`
             + `${item.price}\t`
             + `${item.monthly}\t`
@@ -119,7 +114,6 @@ function tsvDownload() {
     $('#complexTitle').off("click");
     $('#complexTitle').on("click", function() {
         let filename = `${curentApt}.tsv`;
-        console.log(filename);
         downloadTSV(tsv, filename);
     });
 
@@ -152,4 +146,3 @@ const disconnect = VM.observe(document.body, () => {
   
 // You can also disconnect the observer explicitly when it's not used any more
 //disconnect();
-
